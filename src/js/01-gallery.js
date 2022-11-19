8d480cebf0c297e0cf58ea0374a4a5e3e156c1ef
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-19 17:52:14 +0200

    add import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryUl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function makeGallery(arr) {
+  const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}" /></a></li>`;
+    })
+    .join('');
+  return galleryUl.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+}
+
+makeGallery(galleryItems);
+const newGallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
