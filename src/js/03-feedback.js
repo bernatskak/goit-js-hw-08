7da51d43a5363d189bb210393ba769692c716bb6
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-12-04 20:49:48 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,44 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'formData';
 const refs = {
   form: document.querySelector('.feedback-form'),
 };
 
 refs.form.addEventListener('submit', onFormSumbit);
-refs.form.addEventListener('input', throttle(onEmailInput, 500));
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+const STORAGE_KEY = 'formData';
+const formData = {};
+
+getLocalStorageItems();
 
 function onFormSumbit(e) {
   e.preventDefault();
-  const { email, message } = e.currentTarget.elements;
 
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
-  }
-
+  consoleFormData(e.currentTarget);
   e.currentTarget.reset();
-  clearFormData(formData);
   localStorage.removeItem(STORAGE_KEY);
 }
-function clearFormData(obj) {
-  for (const key in obj) {
-    delete obj[key];
-  }
-}
-function onEmailInput(e) {
+
+function onInput(e) {
   formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-function getStorageData() {
-  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (formData) {
-    setFormFields(formData);
-  } else {
-    formData = {};
-  }
-}
-function setFormFields(obj) {
-  for (const key in obj) {
-    refs.form[key].value = obj[key];
-  }
+
+  localStorage.setItem('formData', JSON.stringify(formData));
 }
 
-window.addEventListener('load', getStorageData);
+function getLocalStorageItems() {
+  const storageItem = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (!storageItem) return;
+  Object.keys(storageItem).forEach(key => {
+    formData[key] = storageItem[key];
+    const element = refs.form.querySelector(`[name="${key}"]`);
+    element.value = storageItem[key];
+  });
+}
+
+function consoleFormData(form) {
+  const feedbackData = Object.fromEntries(new FormData(form));
+  return alert('Please fill in all the fields!');
+  console.log('feedbackData', feedbackData);
+}
